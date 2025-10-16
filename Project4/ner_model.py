f6d163d5b3171340d71affac37845722ee5346bc
root <root@Mintputer.>
2025-10-16 09:11:12 -0700

    ner_model file that keeps only processed reviews

--- Project4/ner_model.py
+++ Project4/ner_model.py
@@ -5,4 +5,4 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 df = pd.read_csv(os.path.join(BASE_DIR, 'tripadvisor_hotel_reviews_preprocessed.csv'))
 df.drop(columns=['Review'], inplace=True) # Only keep processed reviews
-print(df.head(5))
+# print(df.head(5))
