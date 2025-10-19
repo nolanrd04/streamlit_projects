705e7680405fc31d76fc0f4a3d11571594add2df
nolanrd04 <nolanrd04@gmail.com>
2025-10-18 18:49:42 -0700

    changed dashboard.

--- Project4/app.py
+++ Project4/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pathlib import Path
 import os
 from ner_model import HotelReviewNLPModel
-from data_preprocessor import download_nltk_resources
+# from data_preprocessor import download_nltk_resources
 
 # Download NLTK resources if not already present
 # download_nltk_resources()
